534bda9760b5b007438a023966f48b48659ef2df
safnahaneefa786 <121495187+safnahaneefa786@users.noreply.github.com>
2023-01-13 09:27:27 +0530

    final

---
+++ Faker_with_3_table.py
@@ -0,0 +1,217 @@
+# Databricks notebook source
+!pip install barnum
+!pip install faker
+
+# COMMAND ----------
+
+from faker import Faker
+import csv
+import random
+from time import time
+from decimal import Decimal
+import pandas as pd
+from datetime import datetime
+import sys
+import random
+import time
+from barnum import gen_data
+import numpy as np
+import re
+fake=Faker()
+
+# COMMAND ----------
+
+df = spark.read.option("delimiter", ",").option("header", True).csv("table.csv")
+df.display()
+df.createOrReplaceTempView('df_table')
+df1 = spark.read.option("delimiter", ",").option("header", True).csv("attribute.csv")
+df1.display()
+df1.createOrReplaceTempView('df1_table')
+
+# COMMAND ----------
+
+def fake_data_create(a,datatype):
+        array =[]
+        for j in range(0,a):
+            if datatype == "number" :
+                array.append(fake.pyint())
+                continue
+            elif datatype== "company_name" :
+                array.append(gen_data.create_company_name())
+                continue
+            elif datatype=="address":
+                st=fake.address()
+                line = st.replace('\n','')
+                array.append(line)
+                continue
+            elif datatype=="country":
+                array.append(fake.country())
+                continue
+            elif datatype=="birthday":
+                array.append(fake.date_of_birth())
+                continue
+            elif datatype=="name": 
+                array.append(fake.name())
+                continue
+            elif datatype=="date":
+                array.append(fake.date())
+                continue
+            elif datatype=="phone":
+                array.append(gen_data.create_phone())
+                continue
+            elif datatype=="email":
+                array.append(gen_data.create_email())
+                continue
+            else:
+                my_list = datatype.split(",")
+                array.append(random.choice((my_list)))
+                continue
+#         print(array)
+        return array
+
+# COMMAND ----------
+
+from pyspark.sql import SparkSession
+import numpy as np
+import pyspark.sql.functions as f
+Details = {}
+primary_array=[]
+path = "{path for saving the csv}"
+spark = SparkSession \
+    .builder \
+    .appName("Python Fake Data App") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+RECORD_COUNT =[]
+#total count of tables
+count_d = spark.sql("select count(table_id) as total_table from df_table")
+total_t = str(count_d.rdd.flatMap(lambda x: x).collect())
+total_tables = re.sub(r"[\([{''})\]]", "", total_t)
+#for loop for creating each table in the csv
+for i in range (1,int(total_tables)+1):
+    filter_value=i
+    #selecting the column names of the table
+    query1 = f'''SELECT column_name
+           FROM df1_table
+           WHERE table_id = {filter_value}'''
+    columns = spark.sql(query1)
+    column_data=columns.rdd.flatMap(lambda x: x).collect()
+    #selecting the format of the columns
+    query2 = f'''select format from df1_table where table_id ={filter_value}'''
+    type = spark.sql(query2)
+    datatype=type.rdd.flatMap(lambda x: x).collect()
+    #selecting the name of the table for saving the csv
+    query3 = f'''SELECT table_name
+           FROM df_table
+           WHERE table_id = {filter_value}'''
+    p = spark.sql(query3)
+    n =str(p.select(f.collect_list('table_name')).first()[0])
+    name = re.sub(r"[\([{''})\]]", "", n)
+   
+    #selecting the count of the current table needed to populate
+    query4 = f'''SELECT count
+                       FROM df_table
+                       WHERE table_id = {filter_value}'''
+    count = spark.sql(query4)
+    RECORD_COUNT=count.rdd.flatMap(lambda x: x).collect()
+    #assigning the count to "a"
+    a= int(RECORD_COUNT[0])
+    #initalise the empty array
+    primary_array = np.empty(a, dtype=object)
+    #selecting the primary column
+    primary = spark.sql(f'''select column_name from df1_table where table_id={filter_value} and is_primary =1''')
+    pr_column = str(primary.rdd.flatMap(lambda x: x).collect())
+    #removing any kind of brackets and qoutes
+    primary_column = re.sub(r"[\([{''})\]]", "", pr_column)
+    #selecting the format of primary column
+    p_type = spark.sql(f'''select format from df1_table where table_id={filter_value} and is_primary =1''')
+    pr_type = str(p_type.rdd.flatMap(lambda x: x).collect())
+    prime_type = re.sub(r"[\([{''})\]]", "", pr_type)
+    #checking the format is number or not
+    if prime_type == 'number':
+        for j in range(0,a):
+            primary_array[j] = int(j+10000)   
+    else :
+        print("no")
+    #converting the data into pandas dataframe
+    panda_df = pd.DataFrame(data = primary_array, 
+                    index = range(0,a), 
+                    columns = [primary_column])
+    #selecting the total count of the columns
+    c_count = spark.sql(f'''select int(count(column_name)) as count from df1_table where table_id={filter_value}''')
+    col_count =str(c_count.rdd.flatMap(lambda x: x).collect())
+    column_count = re.sub(r"[\([{''})\]]", "", col_count)
+    #initalizing the datframe
+    results = pd.DataFrame()
+    #for loop upto the column counts
+    for k in range(0,int(column_count)):
+                    #selecting the each datatype from the array
+                    data_c = datatype[k]
+                    #creating the data by calling the function using the number of count and datatype(format)
+                    data = fake_data_create(a,data_c)
+                    #returning array append to the dataframe
+                    df = pd.DataFrame(data, columns = [column_data[k]])
+                    #concating the dataframe to the result dataframe
+                    results = pd.concat([results, df], axis=1).reset_index(drop=True)  
+    #assigning the unique primary data to the dataframe
+    results[primary_column] = panda_df[primary_column]
+    results.to_csv(path+name +'.csv', index= False)
+#     display(results)
+
+# COMMAND ----------
+
+#selecting the number of foreign keys in the table
+c = spark.sql(f'''select count(foreign_key_id) as count from df1_table where foreign_key_id !=0''')
+co = str(c.rdd.flatMap(lambda x: x).collect())
+count =re.sub(r"[\([{''})\]]", "", co)
+new =[]
+#checking foreign key is empty or not
+if count :
+    #selecting the column names 
+    tg = spark.sql(f'''select column_name from df1_table where foreign_key_id !=0''')
+    tar = tg.rdd.flatMap(lambda x: x).collect()
+    for i in range(0,int(count)):
+        #selecting the one column
+        target_col =tar[i]
+        #source column name for the getting the data
+        pr = spark.sql(f'''select column_name from df1_table where column_id = (select foreign_key_id from df1_table where column_name="{target_col}")''')
+        par = str(pr.rdd.flatMap(lambda x: x).collect())
+        parent_col = re.sub(r"[\([{''})\]]", "", par)
+        #selecting the table name where data should be taken
+        parent = spark.sql(f'''select table_name from df_table where table_id=(select table_id from df1_table where column_name ="{parent_col}")''')
+        parent_n = str(parent.rdd.flatMap(lambda x: x).collect())
+        parent_name = re.sub(r"[\([{''})\]]", "", parent_n)
+        #target table name
+        child = spark.sql(f'''select table_name from df_table where table_id=(select table_id from df1_table where column_name ="{target_col}")''')
+        child_n = str(child.rdd.flatMap(lambda x: x).collect())
+        child_name = re.sub(r"[\([{''})\]]", "", child_n)
+        #read the csv using the names
+        parent_df = pd.read_csv(path+parent_name +'.csv')
+        child_df = pd.read_csv(path+child_name +'.csv')
+        #count of the source table
+        p_count =spark.sql(f'''select Count from df_table where table_name ="{parent_name}"''')
+        pr_count =str(p_count.rdd.flatMap(lambda x: x).collect())
+        parent_count = int(re.sub(r"[\([{''})\]]", "", pr_count))
+        #count of the target table
+        c_count =spark.sql(f'''select Count from df_table where table_name ="{child_name}"''')
+        ch_count =str(c_count.rdd.flatMap(lambda x: x).collect())
+        child_count =int(re.sub(r"[\([{''})\]]", "", ch_count))
+        #selecting the source column as
+        k =parent_df[parent_col].tolist()
+        child_df[target_col] = k *int(child_count / parent_count)
+#         display(child_df)
+        child_df.to_csv(path+child_name +'.csv', index= False)
+else :
+    print("You dont have any foreign key")
+
+# COMMAND ----------
+
+dbutils.fs.rm("{path}order.csv",recurse=True)
+
+# COMMAND ----------
+
+df = spark.read.option("delimiter", ",").option("header", True).csv("{path}/shipper.csv")
+df.display()
+
+
+
